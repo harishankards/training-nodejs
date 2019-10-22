f86e5dad4593c67b36f17f9876d1c38a5d2fdb8a
hs <hs@spritle.com>
2019-10-22 19:49:03 +0530

    Add express app without view

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
-const express = require('express');
+/**
+ * Module dependencies.
+ */
+
+ const express = require('express');
 
 
 /**
  * Create Express server.
  */
 const app = express();
+
+/**
+ * Express configuration.
+ */
+app.set('port', 3000);
+
+
+/**
+ * Start Express server.
+ */
+app.listen(app.get('port'), () => {
+    console.log('App is running at http://localhost:%d', app.get('port'));
+    console.log('  Press CTRL-C to stop\n');
+});
+  
+module.exports = app;
