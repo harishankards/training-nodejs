30b28fbe898b650087f0a24e4c90603808dfb548
hs <hs@spritle.com>
2019-10-22 20:16:46 +0530

    Add conf and connect to mongo

--- app.js
+++ app.js
@@ -3,13 +3,14 @@
  */
 
  const express = require('express');
+ const mongoose = require('mongoose');
+ const path = require('path');
 
 
 /**
  * Create Express server.
  */
 const app = express();
-const path = require('path');
 
 
 /**
@@ -20,6 +21,19 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 
+/**
+ * Connect to MongoDB.
+ */
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true )
+mongoose.connect('mongodb://localhost:27017/training');
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+    console.log('MongoDB connection error. Please make sure MongoDB is running.');
+    process.exit();
+  });
 
 /**
  * Start Express server.
